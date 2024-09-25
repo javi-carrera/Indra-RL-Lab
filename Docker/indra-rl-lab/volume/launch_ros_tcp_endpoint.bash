6b42d1608d57646694c69c83aa0e9aaf66d0810c
Javier Carrera <114339284+javi-carrera@users.noreply.github.com>
2024-09-25 18:40:57 +0200

    Updated bash files to LF

--- Docker/indra-rl-lab/volume/launch_ros_tcp_endpoint.bash
+++ Docker/indra-rl-lab/volume/launch_ros_tcp_endpoint.bash
@@ -1,4 +1,3 @@
 source /opt/ros/humble/setup.bash
 source ~/ros_ws/install/setup.bash
-
 ros2 launch ros_tcp_endpoint ros_tcp_endpoint_launch.py
--- Docker/indra-rl-lab/volume/train.bash
+++ Docker/indra-rl-lab/volume/train.bash
@@ -1,4 +1,3 @@
 source /opt/ros/humble/setup.bash
 source ~/ros_ws/install/setup.bash
-
 ros2 launch rl_pkg train_launch.py
